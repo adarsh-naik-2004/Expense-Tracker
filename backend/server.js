38b62243e6bb9f9932109a62bcb3cb9a601d4924
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-02-02 23:07:04 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -7,20 +7,15 @@ import authRoutes from './routes/authRoutes.js';
 import expenseRoutes from './routes/expenseRoutes.js';
 import { errorHandler } from './middleware/errorHandler.js';
     
-dotenv.config();
-
-app.use(cors({
-  origin: [process.env.CLIENT_URL, "http://localhost:5173"], // Allow frontend URLs
-  credentials: true, // Allow cookies
-  methods: "GET,POST,PUT,DELETE",
-  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-}));
-
-
-app.options("*", cors());
+dotenv.config({
+  path: "./env"
+});
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+const PORT = process.env.PORT;
 
 console.log(process.env.CLIENT_URL);
 
